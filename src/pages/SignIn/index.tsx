a41d6096652bdb9600786ffbdb210d3060fd786b
Junior Trojilio <osmar.trojilio@gmail.com>
2021-09-20 21:55:21 -0300

    feat: add new desacopled components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,17 @@
-import { ReactComponent as Bigfoot } from '../../assets/bear.svg'
 import { Checkbox } from './Checkbox'
 import { Input } from './Input'
+import {ButtonSignIn} from './ButtonSignIn'
+import {SvgContainer} from './SvgContainer'
 import './styles.scss'
-import './styles.svg.scss'
 
 export function SignIn() {
   return (
     <form>
-      <div className="svgContainer">
-        <div>
-          <Bigfoot />
-        </div>
-      </div>
-      <div className="input-group email">
-        <Input label="Email" type="email" name="email" id="email" placeholder=" " required />
-      </div>
-      <div className="input-group">
-        <Input label="Password" type="password" name="password" id="password" required />
-        <Checkbox />
-      </div>
-      <button type="submit" id="login">Log in</button>
+      <SvgContainer />
+      <Input label="Email" type="email" name="email" id="email" placeholder=" " required />
+      <Checkbox id="showPasswordToggle"/>
+      <Input label="Password" type="password" name="password" id="password" required />
+      <ButtonSignIn type="submit">Login</ButtonSignIn>
     </form>
   )
 }
